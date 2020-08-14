bb91138a044b22d15a0f314b44b0b720d9268ce5
Amit Hiremath <avmh200611@gmail.com>
2020-08-14 13:08:55 -0700

    UI works
    UI works. Need to add content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,21 +15,27 @@ export class AppComponent {
   public name = " "; 
   public message =" "; 
   public content = " THIS IS EMPTY"
+  public subheading = "Introduction"
 
   showIntro(){
     this.content = "hello sir my name is amit myself cs major"
+    this.subheading = "Introduction"
   }
   showEdu(){
     this.content = "yo yo i got mad edu bro"
+    this.subheading = "Education"
   }
   showWE(){
     this.content = " I worked at all the places"
+    this.subheading = "Work Experience"
   }
   showAboutMe(){
     this.content = "name: Amit Alter Ego: Hackerman"
+    this.subheading = "About Me"
   }
   showBoi(){
     this.content = "boi."
+    this.subheading = "Boi"
   }
   
 
