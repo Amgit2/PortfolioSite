9c9d374a7b9aee8b1fd8411c62d88710ff1a188c
Amit Hiremath <avmh200611@gmail.com>
2020-09-02 15:07:54 -0700

    made changes to education
    Added buttons. Buttons change titles.

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  schoolName: String;
+  schoolInfo: String; 
 
-  constructor() { }
+  constructor() {
+    this.schoolName ="New York Institute of Technology";
+   }
+
+  nyitClick(){
+      this.schoolName = "New York Institute of Technology"
+  }
+  kuClick(){
+      this.schoolName ="Kettering University"
+  }
+  tisbClick(){
+      this.schoolName = "The International School Bangalore"
+  }
 
   ngOnInit(): void {
   }
